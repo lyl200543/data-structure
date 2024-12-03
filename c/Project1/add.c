779c1efcac502bba8239e70cc2fa91193f171c8b
lyl <13416088820@163.com>
2024-12-03 22:15:24 +0800

    saved

---
+++ c/Project1/add.c
@@ -0,0 +1,13 @@
+#define   _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+ //static int a = 4;
+ //static int add(int x, int y)
+ //{
+	// return x + y;
+ //}
+
+
+int add(int x, int y)
+{
+	return x + y;
+}
